906b2663e4da560c84ef53e9df93db342e6b521f
ENzoram4232 <enzoram36@icloud.com>
2025-07-31 14:23:53 -0400

    filtering function for geniuses cards dropdown animatin for search input in the header for mobile devices and icon implement for the dropdown menu button

--- headerFooter/dropDownMenu.js
+++ headerFooter/dropDownMenu.js
@@ -6,21 +6,9 @@ function toggleDropdown() {
 // Close dropdown if clicking outside container or menu button
 document.addEventListener('click', function (e) {
     const container = document.getElementById('container');
-    const button = document.querySelector('.more-button');
+    
 
-    if (!container.contains(e.target) || !button.contains(e.target)) {
+    if (!container.contains(e.target)) {
         container.classList.remove('expanded');
     }
 });
-
-// Search filter function
-function filterFunction () {
-    const input = document.getElementById('searchInput');
-    const filter = input.value.toUpperCase();
-    const dropdown = document.getElementById('moreDropdown');
-    const links = dropdown.getElementsByTagName('a');
-
-    for (let i = 0; i < links.length; i++) {
-        
-    }
-}
